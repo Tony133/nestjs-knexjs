20a89c107a2097d5f8cb1a18d9fc7834f1779a62
sergeyampo <dev@sergeyampo.ru>
2023-05-04 22:05:05 +0300

    refactor(dependencies): redundant uuid dep, deprecated rxjs method

--- lib/common/knex.utils.ts
+++ lib/common/knex.utils.ts
@@ -2,8 +2,8 @@ import { KnexModuleOptions } from '../interfaces';
 import { knex } from 'knex';
 import { DEFAULT_CONNECTION_NAME } from '../knex.constants';
 import { Observable } from 'rxjs';
-import { delay, retryWhen, scan } from 'rxjs/operators';
-import { v4 as uuid } from 'uuid';
+import { delay, retry, scan } from 'rxjs/operators';
+import { randomUUID } from 'node:crypto';
 import { Logger } from '@nestjs/common';
 import { CircularDependencyException } from '../exceptions/circular-dependency.exception';
 
@@ -62,7 +62,7 @@ export function handleRetry(
 ): <T>(source: Observable<T>) => Observable<T> {
   return <T>(source: Observable<T>) =>
     source.pipe(
-      retryWhen((e) =>
+      retry({ delay: (e) =>
         e.pipe(
           scan((errorCount, error: Error) => {
             logger.error(
@@ -78,7 +78,7 @@ export function handleRetry(
           }, 0),
           delay(retryDelay),
         ),
-      ),
+}),
     );
 }
 
@@ -86,4 +86,4 @@ export function getConnectionName(options: KnexModuleOptions) {
   return options && options.name ? options.name : DEFAULT_CONNECTION_NAME;
 }
 
-export const generateString = () => uuid();
+export const generateString = () => randomUUID();
