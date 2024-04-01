7aa2b63302807aa8525974a1bad7fe4dc9aa8b32
Tony133 <a.tripodi133@gmail.com>
2024-04-01 11:00:55 +0200

    chore: updated

--- tests/src/apps/app-multi-database/app/main.ts
+++ tests/src/apps/app-multi-database/app/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   await app.listen(3000);
 }
